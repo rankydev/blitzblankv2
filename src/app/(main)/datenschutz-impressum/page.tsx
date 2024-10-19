9b3a7a9057b70ca32cc43c8a92de70df03f25f6c
Ranko Spegar <ranko.spegar@gmail.com>
2024-10-19 18:45:51 +0200

    fix typo in datenschutz

--- src/app/(main)/datenschutz-impressum/page.tsx
+++ src/app/(main)/datenschutz-impressum/page.tsx
@@ -7,49 +7,50 @@ const DatenschutzImpressum = () => {
         <h3 className="my-8">Datenschutzerklärung</h3>
         <p>
           In folgender Datenschutzerklärung informieren wir Sie über die
-          wichtigsten Aspekte der Datenverarbeitung im Rahmen unserer Webseite. Wir
-          erheben und verarbeiten personenbezogene Daten nur auf Grundlage der
-          gesetzlichen Bestimmungen (Datenschutzgrundverordnung,
+          wichtigsten Aspekte der Datenverarbeitung im Rahmen unserer Webseite.
+          Wir erheben und verarbeiten personenbezogene Daten nur auf Grundlage
+          der gesetzlichen Bestimmungen (Datenschutzgrundverordnung,
           Telekommunikationsgesetz 2003).
         </p>
         <p>
           Sobald Sie als Benutzer auf unsere Webseite zugreifen oder diese
-          besuchen wird Ihre IP-Adresse, Beginn sowie Beginn und Ende der Sitzung
-          erfasst. Dies ist technisch bedingt und stellt somit ein berechtigtes
-          Interesse iSv Art 6 Abs 1 lit f DSGVO.
+          besuchen wird Ihre IP-Adresse, Beginn sowie Beginn und Ende der
+          Sitzung erfasst. Dies ist technisch bedingt und stellt somit ein
+          berechtigtes Interesse iSv Art 6 Abs 1 lit f DSGVO.
         </p>
         <h5 className="mt-8">Kontakt mit uns</h5>
         <p>
-          Wenn Sie uns, entweder über unser Kontaktformular auf unserer Webseite,
-          oder per Email kontaktieren, dann werden die von Ihnen an uns
-          übermittelten Daten zwecks Bearbeitung Ihrer Anfrage oder für den Fall
-          von weiteren Anschlussfragen für sechs Monate bei uns gespeichert. Es
-          erfolgt, ohne Ihre Einwilligung, keine Weitergabe Ihrer übermittelten
-          Daten.
+          Wenn Sie uns, entweder über unser Kontaktformular auf unserer
+          Webseite, oder per Email kontaktieren, dann werden die von Ihnen an
+          uns übermittelten Daten zwecks Bearbeitung Ihrer Anfrage oder für den
+          Fall von weiteren Anschlussfragen für sechs Monate bei uns
+          gespeichert. Es erfolgt, ohne Ihre Einwilligung, keine Weitergabe
+          Ihrer übermittelten Daten.
         </p>
         <h5 className="mt-8">Google Maps</h5>
         <p>
-          Unsere Website verwendet Funktionen des Webkartendienstes „Google Maps“.
-          Der Dienstanbieter dieser Funktion ist:
+          Unsere Website verwendet Funktionen des Webkartendienstes „Google
+          Maps“. Der Dienstanbieter dieser Funktion ist:
         </p>
         <ul>
           <li>
-            Google Ireland Limited Gordon House, Barrow Street Dublin 4. Ireland.
-            Tel: +353 1 543 1000
+            Google Ireland Limited Gordon House, Barrow Street Dublin 4.
+            Ireland. Tel: +353 1 543 1000
           </li>
         </ul>
         <p>
-          Im Zuge der Nutzung von Google Maps ist es notwendig Ihre IP-Adresse zu
-          speichern und zu verarbeiten. Google überträgt in der Regel an einen
-          Server in den USA und speichert die Daten dort. Die Verarbeitung
-          geschieht durch den Diensteanbieter (oben genannt), der Betreiber dieser
-          Homepage hat keinen Einfluss auf die Übertragung der Daten.
+          Im Zuge der Nutzung von Google Maps ist es notwendig Ihre IP-Adresse
+          zu speichern und zu verarbeiten. Google überträgt in der Regel an
+          einen Server in den USA und speichert die Daten dort. Die Verarbeitung
+          geschieht durch den Diensteanbieter (oben genannt), der Betreiber
+          dieser Homepage hat keinen Einfluss auf die Übertragung der Daten.
         </p>
         <p>
           Die Datenverarbeitung erfolgt auf Basis der gesetzlichen Bestimmungen
-          des § 96 Abs 3 TKG sowie des Art 6 Abs 1 lit f (berechtigtes Interesse)
-          der DSGVO. Die Nutzung von Google Maps erhöht die Auffindbarkeit der
-          Orte, welche auf unserer Webseite bereitgestellt werden.
+          des § 96 Abs 3 TKG sowie des Art 6 Abs 1 lit f (berechtigtes
+          Interesse) der DSGVO. Die Nutzung von Google Maps erhöht die
+          Auffindbarkeit der Orte, welche auf unserer Webseite bereitgestellt
+          werden.
         </p>
         <p>
           Weitere Informationen über den Umgang mit Nutzerdaten des
@@ -62,8 +63,8 @@ const DatenschutzImpressum = () => {
           </a>
         </p>
         <p>
-          Google verarbeitet die Daten auch in den USA, hat sich jedoch dem EU-US
-          Privacy-Shield unterworfen.
+          Google verarbeitet die Daten auch in den USA, hat sich jedoch dem
+          EU-US Privacy-Shield unterworfen.
         </p>
         <p>
           <a href="https://www.privacyshield.gov/EU-US-Framework">
@@ -82,18 +83,20 @@ const DatenschutzImpressum = () => {
         </ul>
         <p>Tel: +353 1 543 1000</p>
         <p>
-          Beim Aufrufen dieser Webseite lädt Ihr Browser Schriftarten und speichert
-          diese in den Cache. Da Sie, als Besucher der Webseite, Daten des
-          Dienstanbieters empfangen kann Google unter Umständen Cookies auf Ihrem
-          Rechner setzen oder analysieren.
+          Beim Aufrufen dieser Webseite lädt Ihr Browser Schriftarten und
+          speichert diese in den Cache. Da Sie, als Besucher der Webseite, Daten
+          des Dienstanbieters empfangen kann Google unter Umständen Cookies auf
+          Ihrem Rechner setzen oder analysieren.
         </p>
         <p>
-          Die Nutzung von „Google-Fonts“ dient der Optimierung unserer Dienstleistung
-          und der einheitlichen Darstellung von Inhalten. Dies stellt ein
-          berechtigtes Interesse im Sinne von Art. 6 Abs. 1 lit. f DSGVO dar.
+          Die Nutzung von „Google-Fonts“ dient der Optimierung unserer
+          Dienstleistung und der einheitlichen Darstellung von Inhalten. Dies
+          stellt ein berechtigtes Interesse im Sinne von Art. 6 Abs. 1 lit. f
+          DSGVO dar.
         </p>
         <p>
-          Weitere Informationen zu Google Fonts erhalten Sie unter folgendem Link:
+          Weitere Informationen zu Google Fonts erhalten Sie unter folgendem
+          Link:
         </p>
         <ul>
           <li>
@@ -103,8 +106,8 @@ const DatenschutzImpressum = () => {
           </li>
         </ul>
         <p>
-          Weitere Informationen über den Umgang mit Nutzerdaten von Google können
-          Sie der Datenschutzerklärung entnehmen:
+          Weitere Informationen über den Umgang mit Nutzerdaten von Google
+          können Sie der Datenschutzerklärung entnehmen:
         </p>
         <ul>
           <li>
@@ -114,8 +117,8 @@ const DatenschutzImpressum = () => {
           </li>
         </ul>
         <p>
-          Google verarbeitet die Daten auch in den USA, hat sich jedoch dem EU-US
-          Privacy-Shield unterworfen.
+          Google verarbeitet die Daten auch in den USA, hat sich jedoch dem
+          EU-US Privacy-Shield unterworfen.
         </p>
         <p>
           <a href="https://www.privacyshield.gov/EU-US-Framework">
@@ -137,11 +140,13 @@ const DatenschutzImpressum = () => {
         </ul>
         <p>
           Wenn Sie vermuten, dass im Zuge der Verarbeitung Ihrer Daten Verstöße
-          gegen das Datenschutzrecht passiert sind, so haben Sie die Möglichkeit,
-          sich bei uns (office@blitzblank-bregenz.at) oder der
+          gegen das Datenschutzrecht passiert sind, so haben Sie die
+          Möglichkeit, sich bei uns (office@blitzblank-bregenz.at) oder der
           Datenschutzbehörde zu beschweren.
         </p>
-        <h5 className="mt-8">Sie erreichen uns unter folgenden Kontaktdaten:</h5>
+        <h5 className="mt-8">
+          Sie erreichen uns unter folgenden Kontaktdaten:
+        </h5>
         <p>
           <b>Webseitenbetreiber:</b> Milan Colic
           <br />
@@ -150,7 +155,7 @@ const DatenschutzImpressum = () => {
           <b>Email:</b> office@blitzblank-bregenz.at
         </p>
         <p>
-          Quelle:{' '}
+          Quelle:{" "}
           <b>
             <a href="https://www.fairesrecht.at/kostenlos-datenschutzerklaerung-erstellen-generator.php">
               Datenschutzgenerator Österreich DSGVO
@@ -160,14 +165,21 @@ const DatenschutzImpressum = () => {
 
         <h3 className="mt-12 mb-8">Impressum</h3>
         <p>
-        <span className="font-bold">Herausgeber</span> Milan Colic<br />
-        <span className="font-bold">Geschäftsinhaber</span>Milan Colic<br />
-        <span className="font-bold">Geschäftsführerin</span>Meri Colic<br />
-        Max-Haller-Straße 2, 6900 Bregenz<br />
-        T +43 5574 43714<br />
-        Zuständiges Finanzamt: Bregenz<br />
-        UID ATU63811049<br />
-        Firmenbuch LG Feldkirch, FN303205 S
+          <span className="font-bold">Herausgeber</span> Milan Colic
+          <br />
+          <span className="font-bold">Geschäftsinhaber</span> Milan Colic
+          <br />
+          <span className="font-bold">Geschäftsführerin</span> Meri Colic
+          <br />
+          Max-Haller-Straße 2, 6900 Bregenz
+          <br />
+          T +43 5574 43714
+          <br />
+          Zuständiges Finanzamt: Bregenz
+          <br />
+          UID ATU63811049
+          <br />
+          Firmenbuch LG Feldkirch, FN303205 S
         </p>
       </div>
     </section>
