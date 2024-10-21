a1372f00b671e2d064a9837a4b21ffff39bffc29
DiviRanky <ranko.spegar@koerber.com>
2024-10-21 13:39:06 +0200

    change favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
         />
 
         {/* favicon */}
-        <link rel="shortcut icon" href={config.site.favicon} />
+        <link rel="icon" href={config.site.favicon} />
         {/* theme meta */}
         <meta name="theme-name" content="nextplate" />
         <meta name="msapplication-TileColor" content="#000000" />
